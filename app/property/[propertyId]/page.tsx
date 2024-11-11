c06a9ca9dcf4e3c78c4e30096c72c3d63f529032
BloggerLife <macarnoldnenyasha@gmail.com>
2024-11-11 06:47:12 +0200

    split1

--- app/property/[propertyId]/page.tsx
+++ app/property/[propertyId]/page.tsx
@@ -1,25 +1,25 @@
-import React, { useEffect, useState } from "react"
-import type { Property } from "@prisma/client"
-import ImageSlider from "./_components/ImageSlider"
-import useBookProperty from "@/hooks/useBookProperty"
-import PropertyDetails from "./_components/PropertyDetails"
-import BookingCard from "./_components/BookingCard"
-import getPoperty from "@/actions/getProperty"
-import getUserInfos from "@/actions/getUserInfos"
-import { getBookings } from "@/actions/getBookings"
+import React, { useEffect, useState } from "react";
+import type { Property } from "@prisma/client";
+import ImageSlider from "./_components/ImageSlider";
+import useBookProperty from "@/hooks/useBookProperty";
+import PropertyDetails from "./_components/PropertyDetails";
+import BookingCard from "./_components/BookingCard";
+import getPoperty from "@/actions/getProperty";
+import getUserInfos from "@/actions/getUserInfos";
+import { getBookings } from "@/actions/getBookings";
 
 const page = async ({ params }: { params: { propertyId: string } }) => {
   //const [property, setProperty] = useState<Property | null>(null)
   //const [images, setImages] = useState<string[]>([])
   //const [userInfos, setUserInfos] = useState<any>(null)
 
-  const property = await getPoperty(params.propertyId)
+  const property = await getPoperty(params.propertyId);
   if (!property) {
-    return <div>Property not found</div>
+    return <div>Property not found</div>;
   }
-  const userInfos = await getUserInfos(property?.userid)
-  const images: string[] = property?.images.split(",")
-  const bookings = await getBookings(params.propertyId)
+  const userInfos = await getUserInfos(property?.userid);
+  const images: string[] = property?.images[0].split(",");
+  const bookings = await getBookings(params.propertyId);
 
   return (
     property && (
@@ -29,7 +29,7 @@ const page = async ({ params }: { params: { propertyId: string } }) => {
         </h1>
         <div className="flex lg:flex-row flex-col gap-5">
           <div className="xl:w-2/3 lg:w-[60%] w-[90%] ">
-            <ImageSlider images={property?.images.split(",")} />
+            <ImageSlider images={property?.images[0].split(",")} />
             {property && (
               <PropertyDetails property={property} userInfos={userInfos} />
             )}
@@ -40,7 +40,7 @@ const page = async ({ params }: { params: { propertyId: string } }) => {
         </div>
       </div>
     )
-  )
-}
+  );
+};
 
-export default page
+export default page;
